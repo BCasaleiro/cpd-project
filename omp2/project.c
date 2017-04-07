44b4bbd3b93444f75a0571e780859eb1028f51a1
aftxavier <aftxavier>
2017-04-07 02:36:43 +0100

    merda

---
+++ omp2/project.c
@@ -0,0 +1,114 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
+#include "octree.h"
+#include <omp.h>
+
+
+coord * leaf_nodes_coords = NULL;
+
+int index1 =0;
+omp_lock_t lck_a;
+omp_lock_t lck_octree;
+
+int one_half[4]={0,3,7,5};
+int other_half[4]={1,2,6,4};
+
+int main(int argc, char* argv[]){
+double start = omp_get_wtime();
+    if(argc<3){
+        printf("Not enough args: requires 2\n");
+        exit(1);
+    }
+
+    FILE * input = fopen(argv[1],"r");
+    
+    if (input == NULL) {
+        printf("Error: file pointer is null.");
+        exit(1);
+    }
+    int seasons = atoi(argv[2]);
+
+    octree  * octree = read_input(input);
+    printf("max depth: %d\n",octree->max_depth);
+    
+    int i = 0;
+    int j = 0;
+    int k = 0;
+    omp_init_lock(&lck_a);
+    omp_init_lock(&lck_octree);
+
+    while(i<seasons){
+        for(j=0;j<8;j++){   //    j indexes root's grand_children
+//printf("\n");
+
+# pragma omp parallel for private(k),firstprivate(j),shared(octree)
+
+            for(k=0;k<4;k++){       
+                char aux_location[octree->max_depth];
+                octree_node * aux=NULL; 
+               // printf("aux=%x\t",&aux);
+                aux_location[0]=octree->root->location;
+                if((octree->root->children[one_half[k]]!=NULL) && (octree->root->children[one_half[k]]->children[j]!=NULL)){
+                    aux = octree->root->children[one_half[k]]->children[j];
+                 //   printf("root->%d->%d\n",k,j);
+                    aux_location[1]= one_half[k];
+                    aux_location[2]= j;
+                    mk_neighborhood(octree, aux, aux_location);
+                }
+            }
+
+# pragma omp parallel for firstprivate(j),shared(octree)
+            for(k=0;k<4;k++){       
+                char aux_location[octree->max_depth];
+                octree_node * aux=NULL; 
+               // printf("aux=%x\t",&aux);
+                aux_location[0]=octree->root->location;
+                if((octree->root->children[other_half[k]]!=NULL) && (octree->root->children[other_half[k]]->children[j]!=NULL)){
+                    aux = octree->root->children[other_half[k]]->children[j];
+                 //   printf("root->%d->%d\n",k,j);
+                    aux_location[1]= other_half[k];
+                    aux_location[2]= j;
+                    mk_neighborhood(octree, aux, aux_location);
+                }
+            }
+        }
+        //mk_neighborhood(octree, octree->root,aux_location);
+        update_octree(octree, octree->root);
+        i++;
+        //printf("%d    leafs: %d\n",i,octree->leaf_population );
+
+//        print_octree(octree,octree->root);
+    }
+
+    char aux_location[octree->max_depth];
+    aux_location[0]=octree->root->location;
+    leaf_nodes_coords = malloc(octree->leaf_population*sizeof(coord));
+    get_leaf_nodes_locations(octree, octree->root ,aux_location);
+    qsort(leaf_nodes_coords,octree->leaf_population, sizeof(coord), larger_coordinate);
+    index1=0;
+
+    FILE * output = fopen("output","w");
+
+    while(index1<octree->leaf_population){
+        fprintf(output,"%d %d %d\n", leaf_nodes_coords[index1].coords[0],leaf_nodes_coords[index1].coords[1],leaf_nodes_coords[index1].coords[2]);
+        
+        index1++;
+ 
+    }
+
+    fclose(output);
+    //free((leaf_nodes_coords));
+    free_octree(octree, octree->root);    
+    free(octree->root->children);
+    free(octree->root->leaf_children);
+    free(octree->root);
+    free(octree);
+
+double end = omp_get_wtime();
+printf("time:%lf\n", end-start);
+    return 1;
+}
+
+
